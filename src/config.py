a6254c79aba34e23d81717ad4dc89e352c55a0fa
phentalex <phentalex@gmail.com>
2024-11-17 15:35:45 +0300

    Trying to solve troubles with CSV and JSON

--- src/config.py
+++ src/config.py
@@ -14,58 +14,76 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 JSON_SAVE_PATH = os.path.join(BASE_DIR, 'JSONsaves')
 CSV_SAVE_PATH = os.path.join(BASE_DIR, 'CSVsaves')
 
-def save_to_json(data, work_id):
+MAX_FILE_SIZE = 100 * 1024 * 1024
+
+
+def append_to_json(data, work_id):
     if not os.path.exists(JSON_SAVE_PATH):
         os.makedirs(JSON_SAVE_PATH)
-    
-    date_str = datetime.now().strftime("%Y-%m-%d")
-    json_filename = os.path.join(JSON_SAVE_PATH, f"{work_id}_{date_str}.json")
-    
-    if not os.path.exists(json_filename):
-        with open(json_filename, 'w', encoding='utf-8') as json_file:
-            json.dump(data, json_file, indent=4, ensure_ascii=False)
+
+    base_filename = datetime.now().strftime("%d_%m_%Y")
+    json_filename = os.path.join(JSON_SAVE_PATH, f"{base_filename}.json")
+
+    if os.path.exists(json_filename) and os.path.getsize(json_filename) >= MAX_FILE_SIZE:
+        date_str = datetime.now().strftime("%d_%m_%Y")
+        file_index = 1
+        json_filename = os.path.join(JSON_SAVE_PATH, f"{base_filename}_{date_str}.json")
+        while os.path.exists(json_filename):
+            json_filename = os.path.join(JSON_SAVE_PATH, f"{base_filename}_{date_str}_{file_index}.json")
+            file_index += 1
+
+    if os.path.exists(json_filename):
+        with open(json_filename, 'r', encoding='utf-8') as json_file:
+            existing_data = json.load(json_file)
+    else:
+        existing_data = []
+
+    if any(entry.get("work_id") == work_id for entry in existing_data):
+        return
+
+    existing_data.append({"work_id": work_id, **data})
+    with open(json_filename, 'w', encoding='utf-8') as json_file:
+        json.dump(existing_data, json_file, indent=4, ensure_ascii=False)
+
 
 def append_to_csv(data, work_id):
     if not os.path.exists(CSV_SAVE_PATH):
         os.makedirs(CSV_SAVE_PATH)
 
-    date_str = datetime.now().strftime("%Y-%m-%d")
-    csv_filename = os.path.join(CSV_SAVE_PATH, f"works_{date_str}.csv")
-    
-    if os.path.exists(csv_filename):
-        with open(csv_filename, 'r', newline='', encoding='utf-8') as csv_file:
-            reader = csv.reader(csv_file)
-            rows = list(reader)
-            
-            if len(rows) >= 100000:
-                csv_filename = os.path.join(CSV_SAVE_PATH, f"works_{date_str}_{len(rows)//100000 + 1}.csv")
+    base_filename = datetime.now().strftime("%d_%m_%Y")
+    csv_filename = os.path.join(CSV_SAVE_PATH, f"{base_filename}.csv")
+
+    file_index = 1
+    if os.path.exists(csv_filename) and os.path.getsize(csv_filename) >= MAX_FILE_SIZE:
+        csv_filename = os.path.join(CSV_SAVE_PATH, f"{base_filename}_{file_index}.csv")
+        file_index += 1
 
     file_exists = os.path.isfile(csv_filename)
     if file_exists:
         with open(csv_filename, 'r', newline='', encoding='utf-8') as csv_file:
             reader = csv.reader(csv_file)
             if any(row[0] == work_id for row in reader):
-                return
+                return 
 
     with open(csv_filename, 'a', newline='', encoding='utf-8') as csv_file:
         writer = csv.writer(csv_file)
         if not file_exists:
-            writer.writerow(data.keys())
-        
+            writer.writerow(['work_id', *data.keys()])
+
         writer.writerow([work_id, *data.values()])
 
 async def fetch_json(url: str):
     async with httpx.AsyncClient() as client:
-        response = await client.get(url, timeout=20)
+        response = await client.get(url, timeout=60)
         if response.status_code == 200:
             return response.json()
         else:
             response.raise_for_status()
 
 def remove_latex_expressions(text):
-    latex_pattern = r'\$'
-    cleaned_text = re.sub(latex_pattern, '', text)
-    return cleaned_text
+    cleaned_text1 = re.sub(r'\\n', '', text) #нужно переделать regex для "\n" 
+    #cleaned_text2 = re.sub(r'\$', '', cleaned_text1)
+    return cleaned_text1
 
 async def get_abstract_text(abstract_inverted_index: dict) -> str:
     word_index = []
--- src/server.py
+++ src/server.py
@@ -1,18 +1,12 @@
 from typing import Annotated, List
-
-import uvicorn
-import httpx
+import httpx, re, uvicorn
 from pydantic import BaseModel
 from fastapi import FastAPI, HTTPException, Body
 from fastapi.middleware.cors import CORSMiddleware
 
 from src.config import logger, public_or_local
 from src.models.valid_type_request import HellowRequest, OpenAlexRequest, WorkRequest
-from src.utils.greeting import hellow_names
-from src.utils.save_fun import save_to_json, append_to_csv
-from src.utils.api_calls import fetch_json
-from config import JSON_SAVE_PATH, CSV_SAVE_PATH, OPENALEX_API_URL
-from config import save_to_json, append_to_csv, fetch_json, get_abstract_text
+from config import append_to_json, append_to_csv, fetch_json, get_abstract_text, OPENALEX_API_URL
 
 if public_or_local == 'LOCAL':
     url = 'http://localhost'
@@ -32,60 +26,17 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-
-@app.post("/template_fast_api/v1/get_works")
-async def inputation(body: Annotated[
-    OpenAlexRequest, Body(
-        example={"ids": ['W144423133', 'W2117692326', 'W2167279371']})]):
-    path_json = JSON_SAVE_PATH
-    path_to_csv = CSV_SAVE_PATH
-    try:
-        ids = body.ids
-        if ids:
-            works = await fetch_json(ids)
-            for id, data in works.items():
-                work_data = {
-                    "id": data.get("id"),
-                    "primary_location": data.get("primary_location"),
-                    "type": data.get("type"),
-                    "publication_year": data.get("publication_year"),
-                    "concepts": data.get("concepts"),
-                    "authorships": data.get("authorships"),
-                    "best_oa_location": data.get("best_oa_location"),
-                    "cited_by_count": data.get("cited_by_count"),
-                    "doi": data.get("doi"),
-                    "locations": data.get("locations"),
-                    "Keywords": data.get("keywords"),
-                    "title": data.get("display_name")
-                    }
-
-                save_to_json(data=work_data, work_id=id, path=path_json)
-                append_to_csv(data=work_data, work_id=id, path=path_to_csv)
-            return works
-        else:
-            logger.error("Something happened during creation of the search table")
-            raise HTTPException(
-                status_code=400,
-                detail="Bad Request",
-                headers={"X-Error": "Something happened during creation of the search table"},
-            )
-    except Exception as ApplicationError:
-        logger.error(ApplicationError.__repr__())
-        raise HTTPException(
-            status_code=400,
-            detail="Unknown Error",
-            headers={"X-Error": f"{ApplicationError.__repr__()}"},
-        )
-
 @app.post("/get_works/")
 async def get_works(body: Annotated[WorkRequest, Body(
-        example={"work_ids": ["W2117692326", "W2502170112", "W0987654321"]}
+        example={"work_ids": ["W2106096361", "W2036113194", "W4211007335", "W1686810756", "W2097706568", "W2101234009", "W2167590372",
+                              "W2117692326", "W1522301498", "W2230728100", "W4297081765", "W2298230951", "W2106787323", "W2101108802",
+                              "W2167279371", "W2163815564", "W2029667189", "W2108234281", "W2125435699", "W4294215472", "W2099540110",
+                              "W1987258130", "W2114918609", "W1791587663", "W2151103935", "W2108598243", "W2103441770", "W2131271579",
+                              "W4235678817", "W2112778345", "W2086957099", "W4385245566", "W3023540311", "W2135943618", "W2087484885",
+                              "W2329395632", "W2912565176", "W2112796928", "W1968834637", "W3001118548", "W2030976617", "W2152311269",
+                              "W2018289835", "W2157823046", "W2120062331", "W2170551349", "W1562208008", "W2142635246", "W1979300931",
+                              "W2011039300"]}
     )]):
-    if len(body.work_ids) > 50:
-        raise HTTPException(
-            status_code=400,
-            detail=f"Maximum number of IDs exceeded. No more than 50 allowed."
-        )
     if len(body.work_ids) > 50:
         raise HTTPException(
             status_code=400,
@@ -99,7 +50,13 @@ async def get_works(body: Annotated[WorkRequest, Body(
         
         try:
             data = await fetch_json(full_id)
-            
+
+            raw_title = data.get("display_name", "")
+            if raw_title is None:
+                cleaned_title = "Untitled"
+            else:
+                cleaned_title = re.sub(r'<[^>]+>', '', raw_title)
+
             work_data = {
                 "id": data.get("id"),
                 "primary_location": data.get("primary_location"),
@@ -112,26 +69,18 @@ async def get_works(body: Annotated[WorkRequest, Body(
                 "doi": data.get("doi"),
                 "locations": data.get("locations"),
                 "Keywords": data.get("keywords"),
-                "title": data.get("display_name")
+                "title": cleaned_title
             }
-            
+            ##
             if 'abstract_inverted_index' in data:
                 abstract_inverted_index = data['abstract_inverted_index']
                 abstract_text = await get_abstract_text(abstract_inverted_index)
             else:
                 abstract_text = "Abstract not found for this work"
-            
+            ##
             work_data["abstract"] = abstract_text
 
-            if 'abstract_inverted_index' in data:
-                abstract_inverted_index = data['abstract_inverted_index']
-                abstract_text = await get_abstract_text(abstract_inverted_index)
-            else:
-                abstract_text = "Abstract not found for this work"
-            
-            work_data["abstract"] = abstract_text
-
-            save_to_json(work_data, id)
+            append_to_json(work_data, id)
             append_to_csv(work_data, id)
             
             results.append(work_data)
